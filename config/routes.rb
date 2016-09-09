29548e18a8ad8aa68b53006453e04ce9e2303490
Patricia Aleshire <woven1web@yahoo.com>
2016-09-09 17:55:09 +0000

    created articles branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   
 
 
+  
  devise_for :users, :controllers => { registrations: 'registrations' }
-
+  resources :articles
   root to: 'pages#index'
 
   get 'pages/contact'
