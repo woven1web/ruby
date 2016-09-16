8467737b5ad09ccfb8ac83fd258b844582d37e4f
Patricia Aleshire <woven1web@yahoo.com>
2016-09-16 17:44:16 +0000

    testing email end for heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'test@example.com'
+  config.mailer_sender = 'do_not_reply@test.com'
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
